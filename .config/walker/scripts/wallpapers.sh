0f6508864c04d7640bce601d33ac4443f4ac5b41
Kexick <trogister.to@gmail.com>
2025-12-22 23:05:35 +0600

    fix(startup): rename drop_down_terminal -> dropdown_terminal; chore(windowRules): change size for dropdown_terminal

--- .config/walker/scripts/wallpapers.sh
+++ .config/walker/scripts/wallpapers.sh
@@ -2,5 +2,5 @@
 Sel=$(find -L "/home/kexick/wallpapers/" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" -o -iname "*.webp" \))
 
 while IFS= read -r path; do
-    echo "label=${path##*/};exec=swww img "$path" -t fade --transition-step 55 --transition-duration 1 --transition-fps 60 --transition-angle 45;wal -t -s -i $path && killall -SIGUSR2 waybar && swaync-client -rs;image=$path;recalculate_score=true;value=$path"
+    echo "label=${path##*/};exec=swww img "$path" -t fade --transition-step 55 --transition-duration 1 --transition-fps 60 --transition-angle 45 --fill-color ffffffff;wal -t -s -i $path && killall -SIGUSR2 waybar && swaync-client -rs;image=$path;recalculate_score=true;value=$path"
 done <<< "$Sel"
